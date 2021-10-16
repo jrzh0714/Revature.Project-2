1180a48da292ba05b6965fdcd30a1d6d9ee157e9
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-10-15 22:29:12 -0400

    YAML and changes in main class

--- src/main/java/com/jteam/project_2/Project2Application.java
+++ src/main/java/com/jteam/project_2/Project2Application.java
@@ -1,13 +1,17 @@
 package com.jteam.project_2;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Project2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Project2Application.class, args);
+
     }
 
     @Bean
