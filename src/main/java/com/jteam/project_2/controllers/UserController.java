d738899cea31f4c558f1eb9a9706be2381af1a0d
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-10-17 16:50:43 -0400

    spring data/hibernate/jackson mapping

--- src/main/java/com/jteam/project_2/controllers/UserController.java
+++ src/main/java/com/jteam/project_2/controllers/UserController.java
@@ -3,7 +3,12 @@ package com.jteam.project_2.controllers;
 import com.jteam.project_2.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Repository
+@RestController
+@RequestMapping(path = "users")
+@Transactional
 public class UserController {
 }
--- src/main/java/com/jteam/project_2/models/UserHistory.java
+++ src/main/java/com/jteam/project_2/models/UserHistory.java
@@ -1,18 +1,25 @@
 package com.jteam.project_2.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.sql.Date;
+import java.util.Objects;
 
 @Table(name = "user_history")
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class UserHistory {
+    // composite key
     @Id
     private int userId;
     @Id
@@ -23,4 +30,17 @@ public class UserHistory {
 
     @Column(name="cooked")
     private boolean cooked;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserHistory that = (UserHistory) o;
+        return Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
