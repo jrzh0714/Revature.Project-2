b785f743ec4c98b3c62d666914288410ec6e613a
Jerry Zheng <jiaruizheng0714@gmail.com>
2021-10-18 20:01:29 -0400

    started working on user object/entity and api-endpoints

--- src/main/java/com/jteam/project_2/controllers/UserController.java
+++ src/main/java/com/jteam/project_2/controllers/UserController.java
@@ -11,10 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "users")
